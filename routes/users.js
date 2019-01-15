68d433bc42bfc0bcf2cee6eaf581cd50ff55b47d
旧梦丶 <329694993@qq.com>
2019-01-15 17:37:16 +0800

    登陆验证

--- routes/users.js
+++ routes/users.js
@@ -10,18 +10,32 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../models/user')
-// var crypto = require('crypto')
+ var crypto = require('crypto')
 // //var movie = require('../models/movie')
 // // var mail = require('../models/mail')
 // // var comment = require('../models/comment')
-// const init_token = 'TKL02O'
+ const init_token = 'TKL02O'
 
 // /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 //用户登录接口
-router.post('/login',function(req,res,next){});
+router.post('/login',function(req,res,next){
+  if(!req.body.username){res.json({status:1,message:"用户名为空"})}
+  if(!req.body.password){res.json({status:1,message:"密码为空"})}
+  user.findUserLogin(req.body.username,req.body.password,function(err,userSave){
+    if(userSave.length !=0){//通过MD5查看密码
+      var token_after = getMD5Password(userSave[0]._id)
+      res.json({
+        status:0,
+        data:{token:token_after,user:userSave},
+        message:"用户登录成功"})
+    }else{
+      res.json({status:1,message:"用户名或者密码错误"})
+  }
+  })
+});
 //用户注册接口
 router.post('/register',function(req,res,next){
   if(!req.body.username){res.json({status:1,message:"用户名为空"})}
@@ -57,5 +71,10 @@ router.post('/sendMail',function(req,res,next){})
 //用户显示站内信,其中的receive参数值为1时是发送的内容,值为2时是收到的内容
 router.post('/receiveMail',function(req,res,next){})
 //获取MD5的值
-function getMD5Password(id){}
+function getMD5Password(id){
+  var md5 = crypto.createHash('md5');
+  var token_before = id+init_token
+  //res.json(userSave[0]._id)
+  return md5.update(token_before).digest('hex')
+}
 module.exports = router;
